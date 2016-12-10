c0d93cb6b130403e9b97cb7e705d3f3cba0a7db7
fogio <fogio010@gmail.com>
2016-12-11 00:03:20 +0100

    update

--- src/MiddlewareProcess.php
+++ src/MiddlewareProcess.php
@@ -4,12 +4,12 @@ namespace Fogio\Util;
 
 class MiddlewareProcess
 {
-    protected $_tasks;
+    protected $_queue;
     protected $_method;
 
     public function __construct($tasks, $method = '__invoke', $params = [])
     {
-        $this->_tasks = $tasks;
+        $this->_queue = $tasks;
         $this->_method = $method;
         foreach ($params as $k => $v) {
             $this->$k = $v;
@@ -18,7 +18,7 @@ class MiddlewareProcess
 
     public function __invoke()
     {
-        $task = array_shift($this->_tasks);
+        $task = array_shift($this->_queue);
         if ($task) {
             $taks->{$this->_method}($this);
         }
@@ -26,16 +26,16 @@ class MiddlewareProcess
         return $this;
     }
 
-    public function unshift($tasks)
+    public function prepend($tasks)
     {
-        $this->_tasks = array_merge($tasks, $this->_tasks);
+        $this->_queue = array_merge($tasks, $this->_queue);
 
         return $this; 
     }
 
-    public function push($tasks)
+    public function append($tasks)
     {
-        $this->_tasks = array_merge($this->_tasks, $tasks);
+        $this->_queue = array_merge($this->_queue, $tasks);
 
         return $this; 
     }
